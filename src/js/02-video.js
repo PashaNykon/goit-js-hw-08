413999a165f4a52672a107ef468975172e6bc798
PashaNykon <pavlonykon@ukr.net>
2023-06-05 13:14:19 +0300

    Перша відправка на перевірку

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,27 @@
-const player = document.querySelector('#vimeo-player');
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onPlayTime, 1000));
+
+function onPlayTime(data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds);
+   }
+
+const currentTime = localStorage.getItem('videoplayer-current-time');
+
+player.setCurrentTime(currentTime)
+    .then(function (seconds) { })
+    .catch(function (error) {
+    switch (error.name) {
+        case 'RangeError':
+          
+            break;
+
+        default:
+            
+            break;
+    }
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,33 @@
+import throttle from 'lodash.throttle';
 
+const formEL = document.querySelector('.feedback-form');
+
+let arrayValues = {};
+formEL.addEventListener('input', throttle(onFormInput, 500));
+formEL.addEventListener('submit', onFormSubmit);
+
+fillFromStorage();
+
+function onFormInput(event) {
+    arrayValues[event.target.name] = event.target.value;
+    localStorage.setItem('feedback-form-state', JSON.stringify(arrayValues));
+}
+
+function fillFromStorage() {
+    let storageValues = JSON.parse(localStorage.getItem('feedback-form-state'));
+    if (storageValues) {
+        if (storageValues.email) {
+            formEL.elements.email.value = storageValues.email;
+        }
+        if (storageValues.message) {
+            formEL.elements.message.value = storageValues.message;
+        }
+    } 
+}
+  
+function onFormSubmit(event) {
+    event.preventDefault();
+    console.log(arrayValues);
+    event.currentTarget.reset();
+    localStorage.clear();
+}
